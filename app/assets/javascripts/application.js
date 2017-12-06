5b89f37b3cebb84672d0aeb50aa9ff7803903f89
Mike Deiters <miked49er@gmail.com>
2017-12-05 22:08:39 -0500

    Finished Toy App

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,4 @@
 //= require bootstrap
 //= require rails-ujs
 //= require turbolinks
-//= require_tree .
+//= require_tree
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,11 @@ module SessionsHelper
 
   # Returns true id the user is logged in, false otherwise
   def logged_in?
+    !current_user.nil?
+  end
+
+  # Logs out the current user.
+  def log_out
     session.delete(:user_id)
     @current_user = nil
   end
